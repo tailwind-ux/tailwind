668450fb455d55cb06a774bdeede996000cd3d22
unknown <03142006036sami@gmail.com>
2022-10-05 12:17:31 +0500

    notifications

---
+++ index.js
@@ -0,0 +1,5 @@
+// setTimeout(() => {
+//     location.href = location.href
+// }, 5000);
+const notificationComponent = document.querySelector('.notificationComponent');
+// notificationComponent.scrollIntoView()
